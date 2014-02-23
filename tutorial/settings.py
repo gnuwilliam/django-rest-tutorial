1e7b970260a2bc24723f9060bb9bf6412e55607d
William Oliveira <sudowilliam@gmail.com>
2014-02-23 19:17:33 -0300

    syncdb

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -58,7 +58,11 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database.sql'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': ''
     }
 }
 
