741271e8e1fa08053ba54b3879a72995da8bd2d5
William Oliveira <sudowilliam@gmail.com>
2014-02-24 12:09:31 -0300

    finish serializer

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,7 +2,11 @@ from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
-class SnippetSerializer(serializers.Serializer):
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        
     pk = serializers.Field() # Note: `Field` is an untyped read-only field.
     title = serializers.CharField(required=False,
                                   max_length=100)
