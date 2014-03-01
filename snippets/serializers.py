06ca9ed86c2c0575e542551a52e68e504d44d797
William Oliveira <sudowilliam@gmail.com>
2014-03-01 11:55:06 -0300

    adjusting spaces

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,19 +4,15 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        model   = Snippet
+        fields  = ('id', 'title', 'code', 'linenos', 'language', 'style')
         
-    pk = serializers.Field() # Note: `Field` is an untyped read-only field.
-    title = serializers.CharField(required=False,
-                                  max_length=100)
-    code = serializers.CharField(widget=widgets.Textarea,
-                                 max_length=100000)
-    linenos = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES,
-                                       default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES,
-                                    default='friendly')
+    pk         = serializers.Field() # Note: `Field` is an untyped read-only field.
+    title      = serializers.CharField(required=False,max_length=100)
+    code       = serializers.CharField(widget=widgets.Textarea,max_length=100000)
+    linenos    = serializers.BooleanField(required=False)
+    language   = serializers.ChoiceField(choices=LANGUAGE_CHOICES,default='python')
+    style      = serializers.ChoiceField(choices=STYLE_CHOICES,default='friendly')
 
     def restore_object(self, attrs, instance=None):
         """
@@ -28,11 +24,11 @@ class SnippetSerializer(serializers.ModelSerializer):
         """
         if instance:
             # Update existing instance
-            instance.title = attrs.get('title', instance.title)
-            instance.code = attrs.get('code', instance.code)
-            instance.linenos = attrs.get('linenos', instance.linenos)
-            instance.language = attrs.get('language', instance.language)
-            instance.style = attrs.get('style', instance.style)
+            instance.title      = attrs.get('title', instance.title)
+            instance.code       = attrs.get('code', instance.code)
+            instance.linenos    = attrs.get('linenos', instance.linenos)
+            instance.language   = attrs.get('language', instance.language)
+            instance.style      = attrs.get('style', instance.style)
             return instance
 
         # Create new instance
